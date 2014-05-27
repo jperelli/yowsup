d6b668145237ee304d9e7f273a70159b4a5d6745
jperelli <jperelli@gmail.com>
2014-05-27 02:19:11 -0300

    refactor all to push to pypi register

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'yowsup',
+  packages = ['yowsup'],
+  version = '0.1',
+  description = 'A library to use whatsapp from python, and a cli tool',
+  url = 'https://github.com/jperelli/yowsup',
+  download_url = 'https://github.com/jperelli/yowsup/tarball/0.1',
+  keywords = ['whatsapp', 'API', 'wrapper'. 'cli'],
+  classifiers = [],
+)
