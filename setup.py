d6c45bcc6c7fbceb87e9e55f0eb51f886e925b18
jperelli <jperelli@gmail.com>
2014-05-27 02:55:25 -0300

    refactoring again to pypi better

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
   version = '0.1',
   maintainer = 'Julian Perelli',
   maintainer_email = 'jperelli@gmail.com',
-  description = 'A library to use whatsapp from python, and a cli tool',
+  description = 'A library to use whatsapp from python',
   url = 'https://github.com/jperelli/yowsup',
   download_url = 'https://github.com/jperelli/yowsup/tarball/0.1',
-  keywords = ['whatsapp', 'API', 'wrapper', 'cli'],
+  keywords = ['whatsapp', 'API', 'wrapper'],
   classifiers = [],
 )
